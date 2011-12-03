0c64ef45d64d25e8b70527bab18e4652a4bd3af5
nadams <nadams@student.neumont.edu>
2011-12-03 10:38:51 -0700

    Abstracted away all of the enum values from the view models

---
+++ SkulltagConfGenerator.Domain/Model/IFlagWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkulltagConfGenerator.Domain.Model {
+	public interface IFlagWrapper<T> {
+		IEnumerable<IFlagWrapper<T>> GetAllFlags();
+		IEnumerable<IFlagWrapper<T>> GetFlags(int value);
+		string GetAlternateName();
+		string GetStringValue();
+		int GetValue();
+	}
+}
