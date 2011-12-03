a6aa40578907608992f665be12d2a04a657a8ad8
nadams <nadams@student.neumont.edu>
2011-12-03 08:25:56 -0700

    Implemented DMFlags2ViewModel

---
+++ SkulltagConfGenerator.GUI/UserControls/DMFlags2UserControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkulltagConfGenerator.GUI.UserControls {
+	/// <summary>
+	/// Interaction logic for DMFlags2UserControl.xaml
+	/// </summary>
+	public partial class DMFlags2UserControl : UserControl {
+		public DMFlags2UserControl() {
+			InitializeComponent();
+		}
+	}
+}
