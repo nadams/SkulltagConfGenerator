f50a8c518ae785a34195c0f73e83689c8dad8c65
nadams <nadams@student.neumont.edu>
2011-12-03 19:54:41 -0700

    Added the rest of the flag models

---
+++ SkulltagConfGenerator.GUI/Model/CompatFlag2.cs
@@ -0,0 +1,10 @@
+﻿using SkulltagConfGenerator.Enumerations;
+using SkulltagConfGenerator.Enumerations.Utils;
+
+namespace SkulltagConfGenerator.GUI.Model {
+	public class CompatFlag2 : Flag {
+		public CompatFlag2() { }
+
+		public CompatFlag2(CompatFlags2 flag) : base((int)flag, flag.GetFirstAlternateName(), flag.GetStringValue()) { }
+	}
+}
