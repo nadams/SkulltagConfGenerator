9f2d0f572560faa3d68827a3a1f5c842bf275ccd
nadams <nadams@student.neumont.edu>
2011-12-03 19:49:24 -0700

    Added view models

---
+++ SkulltagConfGenerator.GUI/ViewModel/DMFlagsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SkulltagConfGenerator.Enumerations;
+using SkulltagConfGenerator.GUI.Model;
+using SkulltagConfGenerator.Domain.Model;
+
+namespace SkulltagConfGenerator.GUI.ViewModel {
+	public class DMFlagsViewModel : FlagViewModel<DMFlag, DMFlags> {
+		public DMFlagsViewModel() : base(new DMFlagsWrapper()) { }
+	}
+}
