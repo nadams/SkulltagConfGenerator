f4b3f05c4a93bb13d5eaa70fde6fabcab99632e8
nadams <nadams@student.neumont.edu>
2011-12-03 20:02:59 -0700

    Added the rest of the flag view models

---
+++ SkulltagConfGenerator.GUI/ViewModel/CompatFlagsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SkulltagConfGenerator.GUI.Model;
+using SkulltagConfGenerator.Enumerations;
+using SkulltagConfGenerator.Domain.Model;
+
+namespace SkulltagConfGenerator.GUI.ViewModel {
+	public class CompatFlagsViewModel : FlagViewModel<CompatFlag, CompatFlags> {
+		public CompatFlagsViewModel() : base(new CompatFlagsWrapper()) { }
+	}
+}
---
+++ SkulltagConfGenerator.GUI/ViewModel/DMFlags3ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SkulltagConfGenerator.GUI.Model;
+using SkulltagConfGenerator.Enumerations;
+using SkulltagConfGenerator.Domain.Model;
+
+namespace SkulltagConfGenerator.GUI.ViewModel {
+	public class DMFlags3ViewModel : FlagViewModel<DMFlag3, DMFlags3> {
+		public DMFlags3ViewModel() : base(new DMFlags3Wrapper()) { }
+	}
+}
